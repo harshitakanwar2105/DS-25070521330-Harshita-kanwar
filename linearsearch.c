baa1c1c08370041158a3ec8d861db9ca9d5af548
harshitakanwar2105 <harshitakanwar2105@gmail.com>
2026-02-16 10:02:30 +0530

    linearsearch.c

--- linearsearch.c
+++ linearsearch.c
@@ -1,1 +1,27 @@
-
+#include<stdio.h>
+void main(){
+    int i, n, target, flag = 0;
+    scanf("%d", &n);
+    int A[n];
+    
+    
+    for(i=0; i<n; i++){
+        scanf("%d", &A[i]);  
+    }
+    
+    
+    scanf("%d", &target); 
+    
+    
+    for(i=0; i<n; i++){
+        if(target == A[i]){
+            printf("found at position %d\n", i);
+            flag = 1;
+            break;
+        }
+    }
+    
+    if(flag == 0){
+        printf("%d not found\n", target);  
+    }
+}
