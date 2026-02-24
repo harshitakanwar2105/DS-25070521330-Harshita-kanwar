404ed182e02330357d98a5fc9186baf9f3424361
harshitakanwar2105 <harshitakanwar2105@gmail.com>
2026-02-24 22:58:16 +0530

    insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 
-void insertionSort(int a[], int n) {
-   int i,key,j;
-	for(i=1;i<n;i++){
-		key = a[i];
-		j=i-1;
-
-		while(j>=0 && a[j]>key){
-			a[j+1]=a[j];
-			j--;
-		}
-		a[j+1]=key;
-	}
-    
-}
-
 int main() {
-    int a[20], n, i;
+    int a[100], n, i, j, key;
 
+    printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    insertionSort(a, n);
+    for(i = 1; i < n; i++) {
+        key = a[i];     
+        j = i - 1;
 
-    for (i = 0; i < n; i++) {
+        while(j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = key; 
+    }
+
+    printf("Sorted array:\n");
+    for(i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
 
